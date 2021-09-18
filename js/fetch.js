74129269f640ba4e80c1a5d6d382be8965e91983
jeff123wang <53447732+jeff123wang@users.noreply.github.com>
2021-09-17 22:47:05 -0400

    Update fetch.js

--- js/fetch.js
+++ js/fetch.js
@@ -1,14 +1,8 @@
-fetch("https://ajith-holy-bible.p.rapidapi.com/GetVerses?Book=Luke&chapter=1&VerseFrom=5&VerseTo=8", {
+export const getData = () =>fetch("https://ajith-holy-bible.p.rapidapi.com/GetVerses?Book=Luke&chapter=1&VerseFrom=5&VerseTo=8", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "ajith-holy-bible.p.rapidapi.com",
 		"x-rapidapi-key": "e2fab55c7cmsh15ded68d2a35c2bp1f1b6ajsne09ff8fa152e"
 	}
 })
-.then(response => {
-	return response.json();
-})
-.then(console.log(json))
-.catch(err => {
-	console.error(err);
-});
+.then(response => response.json()) 
