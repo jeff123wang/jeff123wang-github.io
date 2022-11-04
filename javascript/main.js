4faa4264bf4a70a38d10c71281d436f69469244d
jeff123wang <applewang1990@gmail.com>
2022-11-03 20:15:20 -0400

    added p5 canvas

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,18 @@
 function hello() {
     alert("hello");
 }
+
+function sketch_idnameofdiv(p) {
+    p.setup = function () {
+        p.createCanvas(400, 400);
+        p.background(200);
+        p.noLoop();
+
+    }
+
+    p.draw = function () {
+        // stuff to draw
+        p.circle(50, 50, 20, 20);
+    }
+}
+new p5(sketch_idnameofdiv, 'canvas')
